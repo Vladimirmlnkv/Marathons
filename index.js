6593c1abad93bc7cf5e025c6f1dffd5a9cf343d6
Владимир Мельников <vladimirmelnikov@Vladimirs-MacBook-Pro.local>
2017-05-09 13:59:14 +0400

    Created local api

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 var express = require('express')
 var app = express()
+var request = require('request')
 
 app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + "/build"))
 
-app.get('*', function(request, response) {
+app.get('/', function(request, response) {
     response.sendFile(__dirname + '/build/index.html')
+})
+
+app.get('/api/cities', (req, resp) => {
+    var url = "http://api.travelpayouts.com/data/cities.json?token=37813cee19f8d2cc2635c26382fddb24"
+    request(url, (error, response, body) => {
+        resp.send(body)
+    })
+})
+
+app.get('/api/getId', (req, resp) => {
+    var city = req.query.city
+    let url = "http://engine.hotellook.com/api/v2/lookup.json?query="+city+"&lang=ru&lookFor=both&limit=1"
+    request(url, (error, response, body) => {
+        var jsonBody = JSON.parse(body)
+        var id = jsonBody.results.locations[0].id
+        resp.json({id: id})
+    })
 })
 
 app.listen(app.get('port'), function() {
--- src/servises/AviasalesApi.js
+++ src/servises/AviasalesApi.js
@@ -1,21 +1,17 @@
 class AviasalesApi {
 
     fetchCities() {
-        return fetch("http://api.travelpayouts.com/data/cities.json", {
-            headers: {
-                "X-Access-Token": "37813cee19f8d2cc2635c26382fddb24",
-            },
-        })
+        return fetch("/api/cities")
         .then((data) => { return data.json() })
         .then((data) => { return data})
         .catch((error) => {console.log(error)})
     }
 
     getCityId(city) {
-        var url = "http://engine.hotellook.com/api/v2/lookup.json?query="+city+"&lang=ru&lookFor=both&limit=1"
+        var url = "/api/getId?city=" + city
         return fetch(url)
         .then((data) => {return data.json()})
-        .then((data) => {return data.results.locations[0].id})
+        .then((data) => {console.log(data); return data.id})
         .catch((error) => {console.log(error)})
     }
 
